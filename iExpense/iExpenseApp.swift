8a73cd2733e2c9558c683bedb8dbfa442ea8c2c1
Brittany Pollock <brittpollock15@gmail.com>
2026-02-07 11:56:35 -0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Brittany Pollock on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
